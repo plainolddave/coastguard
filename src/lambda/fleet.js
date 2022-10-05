4495e74417ddb7c4e3dad22c87952fd4b21f70e4
plainolddave <2941290+plainolddave@users.noreply.github.com>
2022-10-05 12:30:08 +1000

    Update fleet.js

--- src/lambda/fleet.js
+++ src/lambda/fleet.js
@@ -1,12 +1,11 @@
 import axios from "axios"
-const API_ENDPOINT = "https://proxy.cors.sh/https://data.mongodb-api.com/app/data-xplts/endpoint/fleet_activity";
+const API_ENDPOINT = "https://ap-southeast-2.aws.data.mongodb-api.com/app/signalk-jtrvz/endpoint/fleet";
 const API_CONFIG = {
     headers: {
-        'api-key': 'ge7IDbWs23g84J2smkY82GP0waJUjRz1YngcZPQkmL0zpfUTx8meeOS3Rk7RCvcR',
+        'api-key': 'Eik2e3WiQpKligCnVUJEOPtav9oUUwN8Ai4qEUmICdSc27Q9R6BmAiP94kaNekKi',
         'Accept': 'application/json'
     }
 }
-
 export async function handler(event, context) {
 
     try {
@@ -15,14 +14,13 @@ export async function handler(event, context) {
             return { statusCode: 405, body: "Method Not Allowed" };
         }
 
-        //let url = `${API_ENDPOINT}?path=${event.queryStringParameters.field || "vessels"}`;
-        //if (event.queryStringParameters.from) { url += `&from=${event.queryStringParameters.from}` };
-        //if (event.queryStringParameters.to) { url += `&to=${event.queryStringParameters.to}` };
-        //url += `&limit=${event.queryStringParameters.limit || "1000"}`;
+        let url = API_ENDPOINT + `?limit=${event.queryStringParameters.limit || "1000"}`;
+        if (event.queryStringParameters.from) { url += `&from=${event.queryStringParameters.from}` };
+        if (event.queryStringParameters.to) { url += `&to=${event.queryStringParameters.to}` };
 
-        const response = await axios.get(API_ENDPOINT, API_CONFIG);
+        const response = await axios.get(url, API_CONFIG);
         const data = JSON.stringify(response.data);
-/*        console.log(`url: ${url} response: ${data}`) */
+        console.log(`url: ${url} response: ${data}`)
 
         return {
             statusCode: 200,
@@ -34,7 +32,7 @@ export async function handler(event, context) {
 
     } catch (err) {
 
-        console.log(err) 
+        console.log(err)
         return {
             statusCode: 500,
             headers: {
