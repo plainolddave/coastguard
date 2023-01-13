470e0b62130a2bd6b8dce17314ab32b9f6f10440
plainolddave <2941290+plainolddave@users.noreply.github.com>
2023-01-13 20:57:59 +1000

    CICD enabled for MongoDB

---
+++ mongo/functions/api_get_fleet.js
@@ -0,0 +1,179 @@
+// Send back an error message
+const handleError = (code, headers, error) => {
+  response.setStatusCode(code);
+  response.setHeader("Content-Type","application/json");
+  response.setBody(JSON.stringify({"code": code, "error": JSON.stringify(error)}));
+  context.functions.execute("api_log", "api_get_fleet", code, headers, error);
+}
+
+// This function is the endpoint's request handler.
+exports = function({ query, headers, body}, response) {
+
+  try {
+ 
+    // run an aggregation pipeline to get the requested data
+    const collection = context.services.get("mongodb-atlas").db("ais").collection("positions");
+    var pipeline = [];
+    var result = { 'tracks': [] };
+    
+    //result["query"] = query;
+    
+    // optional - unix timestamp from which to include in the response
+    var _from = new Date("1970-01-01T00:00:00Z")
+    if (query["from"]) {
+      const _fromStr = String(query["from"]);
+      const _fromVal = parseInt(_fromStr);
+      if(!isNaN(_fromVal)) {
+        _from = new Date(_fromVal * 1000);
+      }
+    }
+    result['from'] = _from;
+        
+    // optional - unix timestamp to which to include in the response
+    var _to = new Date()
+    if (query["to"]) {
+      const _toStr = String(query["to"]);
+      const _toVal = parseInt(_toStr);
+      if(!isNaN(_toVal)) {
+        _to = new Date(_toVal * 1000);
+      }
+    }
+    result['to'] = _to;
+    
+    // optional - minimum sog to include in the response
+    var _sog =0;
+    if (query["sog"]) {
+      const sogStr = String(query["sog"]);
+      const sogVal = parseFloat(sogStr);
+      if(!isNaN(sogVal)) {
+        _sog = sogVal;
+      }
+    }
+    result['sog'] = _sog;
+        
+    // add the inial filters to the pipeline then sort
+    pipeline.push({"$match": { "time" : { $gte:  _from, $lte:  _to}, "sog": { $gte:  _sog} }});
+
+    //get rid of outliers
+    pipeline.push({"$match": { "mmsi" : { $ne:  135497088}}});  
+
+    // group records into bins of n minutes (default 1 min)
+    var mins = 1;
+    if (query["mins"]) {
+      const minsStr = String(query["mins"]);
+      const minsVal = parseInt(minsStr);
+      if(!isNaN(minsVal)) {
+        mins = minsVal;
+        result['mins'] = minsVal;
+      }
+    }
+    pipeline.push({ $set: { one_minute: { $dateTrunc: { date: '$time', unit: 'minute', binSize: mins } } } });
+    pipeline.push({ $group: {
+        _id: { time_bin: '$one_minute', mmsi_bin: '$mmsi' },
+        name: { $last: '$vessel.name' },
+        lat: { $last: { $arrayElemAt: ['$pos.coordinates', 1] } },
+        lon: { $last: { $arrayElemAt: ['$pos.coordinates', 0] } },
+        cog: { $avg: '$cog' },
+        sog: { $max: '$sog' }
+    }});
+
+    pipeline.push({"$match": { "lat" : { $gte: -90.0, $lte: 90.0}}});     
+    pipeline.push({"$match": { "lon" : { $gte: -180.0, $lte: 180.0}}});  
+    
+    // fix the rounding for lat/lon
+    pipeline.push({ $set: {
+        lat: { $round: ['$lat', 6] },
+        lon: { $round: ['$lon', 6] },
+        time: { $toLong: '$_id.time_bin' },
+        mmsi: '$_id.mmsi_bin'
+    }});  
+      
+    // group positions per vessel
+    pipeline.push({ $group: {
+        _id: { mmsi_bin: '$mmsi' },
+        mmsi: { $last: '$mmsi' },
+        name: { $last: '$name' },
+        dt: { $last: { $divide: [ '$time' , 1000 ] } },
+        track: { $addToSet: {dt: { $divide: [ '$time', 1000 ] }, lat: '$lat', lon: '$lon', cog: { $round: [ '$cog', 0 ] }, sog: { $round: [ '$sog', 0 ] } }}
+    }});  
+    pipeline.push({ $unset: '_id' }); 
+    pipeline.push({ $lookup: {
+      from: "vessels",
+      localField: "mmsi",
+      foreignField: "mmsi",
+      as: "vessel"
+    }});
+
+    // clean up entries without a match to a known vessel
+    // also sort the posiions by time
+    pipeline.push({ $set: {
+      info: { 
+        $cond: { 
+          if: { $gt: [{ $size: "$vessel" }, 0 ]},
+          then: { $arrayElemAt: [ "$vessel", 0 ]}, 
+          else: {
+            name: {$toString: "$mmsi"},
+            mmsi: "$mmsi",
+            fleet: "Other",
+            org: "Other",
+            color: "gray",
+            style: {
+              zIndex: 100,
+              vesselColor: "gray",
+              fleetColor: "gray",
+              orgColor: "gray"
+            }
+      }}}
+    }});
+    pipeline.push({ $unset: ['info._id', 'vessel'] });
+
+    // optional - filter the vessel org(s) to include in the response
+    // TODO TODO this is hardcoded - should be more flexible
+    // allowable options are: 
+    // QF2 - retrieves QF2 only
+    // SAR - retrieves QF2, AVCG, VMR
+    // ALL - applies no filter
+    result.org = { 
+      options: [{ value: 'QF2', label: 'QF2 Brisbane' }, { value: 'SAR', label: 'Marine Rescue' }, { value: 'ALL', label: 'All Vessels' }],
+      value: 2
+    };
+    if (query["org"]) {
+      const orgStr = String(query["org"]);
+      if(orgStr === "QF2") {
+        pipeline.push({$match: {"info.fleet": 'QF2 Brisbane'}});
+        result.org.value = 0;
+      } else if (orgStr === "SAR") {
+        pipeline.push({$match: {"info.org": { $in: ['QF2', 'VMR', 'AVCG'] }}});
+        result.org.value = 1;
+      } 
+    }
+    
+    // optional - limit to a cap of vessels
+    if (query["limit"]) {
+      const limitStr = String(query["limit"]);
+      const limitVal = parseInt(limitStr);
+      if(!isNaN(limitVal)) {
+        pipeline.push({"$limit": limitVal});
+        result['limit'] = limitVal;
+      }
+    }
+          
+    // run the aggregation pipeline
+    return collection.aggregate(pipeline)
+    .toArray()
+    .then(tracks => {
+      // send back the result
+      result.tracks = tracks;
+      response.setStatusCode(200);
+      response.setHeader("Content-Type","application/json");
+      response.setBody(JSON.stringify(result));
+      context.functions.execute("api_log", "api_get_fleet", 200, headers);
+    })
+    .catch(error => {
+      handleError(422, headers, error);
+    });
+    
+  } catch (error) {
+    handleError(400, headers, error);
+  }
+};
---
+++ mongo/functions/openweathermap_get_observation.js
@@ -0,0 +1,95 @@
+// Get the weather for Manly from openweathermap.org - expected return is:
+// {
+//    "coord":{"lon":153.19,"lat":-27.45},
+//    "weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"}],
+//    "base":"stations",
+//    "main":{"temp":289.74,"feels_like":289.86,"temp_min":288.8,"temp_max":290.95,"pressure":1015,"humidity":92},
+//    "visibility":10000,
+//    "wind":{"speed":3.09,"deg":330},
+//    "clouds":{"all":75},
+//    "dt":1662729529,
+//    "sys":{"type":1,"id":9485,"country":"AU","sunrise":1662666760,"sunset":1662709036},
+//    "timezone":36000,
+//    "id":2158868,
+//    "name":"Manly",
+//    "cod":200
+// }
+
+exports = function(){
+
+  // constants - just get weather for Manly Queensland 
+  const lat = -27.45;
+  const lon = 153.20;
+  const key = context.values.get("OPENWEATHERMAP_API_KEY_VALUE");
+  const httpService = context.services.get('http');
+
+  // helper functions
+  function titleCase(str) {
+    return str.toLowerCase().split(' ').map(function(word) {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ');
+  }
+  
+  function roundDecimal(num, precision) {
+    return Math.round((num + Number.EPSILON) * Math.pow(10,precision)) / Math.pow(10,precision);
+  }
+  
+  function dateLabel(dt_secs, offset_hrs)
+  {
+    return new Date((dt_secs - offset_hrs*60*60)*1000)
+      .toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit', hour12: false});
+  }
+  
+  // retrieve the latest weather
+  let url_str = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${key}`;    
+  context.http.get({ url: url_str })
+  .then(response => {
+    
+    // check if there is already a record for this time in the collection
+    let json = JSON.parse(response.body.text());
+    json.time = new Date(json.dt * 1000);
+    const collection = context.services.get("mongodb-atlas").db("weather").collection("observation");
+    collection.findOne({ time: json.time }).then(existing => {
+
+      //console.log(`response: ${JSON.stringify(json)}`);
+      //console.log(`existing: ${JSON.stringify(existing)}`);
+
+      // if the doc already exists, do nothing
+      if (existing) { return; }
+
+      // build a new document to make it easier to manage the API
+      let doc = {};
+      doc.all = json;
+      doc.time = new Date(json.dt * 1000);
+      doc.dt = json.dt;
+      doc.weather = { icon: json.weather[0].icon, label: titleCase(json.weather[0].description), temp: json.main.temp};
+      doc.wind = { knots: roundDecimal(json.wind.speed * 1.94384, 2), direction: json.wind.deg };
+      //doc.wind = { knots: Math.Round(json.wind.speed * 1.94384), direction: json.wind.deg };
+      doc.stats = {
+        sunrise_dt: json.sys.sunrise, 
+        //sunrise_label: dateLabel(json.sys.sunrise, -10),
+        sunset_dt: json.sys.sunset, 
+        //sunset_label: dateLabel(json.sys.sunset, -10),
+        cloud : json.clouds.all,
+        pressure: json.main.pressure,
+        humidity: json.main.humidity,
+        temp: json.main.temp
+      };
+      doc.pressure = json.main.pressure;
+      doc.temp = json.main.temp;
+      doc.humidity = json.main.humidity;     
+      doc.cloud = json.clouds.all;
+
+      return collection.insertOne(doc)
+      .then(result => {
+        console.log(`Weather ${JSON.stringify(doc)}`);
+      })
+      .catch(error => {
+        console.error(`Weather error: ${error}`);
+      });
+    });
+  })
+  .catch(error => {
+    console.error(`Weather error: ${error}`);
+  });
+};
